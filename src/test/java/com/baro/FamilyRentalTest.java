c27ba490eaff4f1b75159c637ecdf61cb4ab0ae4
Martin Barotto <barottomartin@outlook.com>
2018-10-10 19:30:37 -0300

    Organize tests and add coverage

---
+++ src/test/java/com/baro/FamilyRentalTest.java
@@ -0,0 +1,47 @@
+package com.baro;
+
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FamilyRentalTest {
+
+    @Test(expected = IllegalRentalException.class)
+    public void createIllegalFamilyRentalByInsufficiency() throws IllegalRentalException {
+        Instant startTime = Instant.now();
+        List<SingleRental> rentals = new ArrayList<>();
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        new FamilyRental(rentals);
+    }
+
+    @Test(expected = IllegalRentalException.class)
+    public void createIllegalFamilyRentalByExcess() throws IllegalRentalException {
+        Instant startTime = Instant.now();
+        List<SingleRental> rentals = new ArrayList<>();
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
+        new FamilyRental(rentals);
+    }
+
+    @Test
+    public void accurateFamilyRentalPricing() throws IllegalRentalException {
+        Instant startTime = Instant.now();
+        List<SingleRental> rentals = new ArrayList<>();
+        rentals.add(new SingleRental(startTime, startTime.plus(4, ChronoUnit.HOURS)));
+        rentals.add(new SingleRental(startTime, startTime.plus(3, ChronoUnit.DAYS)));
+        rentals.add(new SingleRental(startTime, startTime.plus(14, ChronoUnit.DAYS)));
+        FamilyRental familyRental = new FamilyRental(rentals);
+        // Estimated price = ( (4*5 + 3*20 + 2*60) * 0.7)
+        assertEquals(140d, familyRental.getPrice(), 0d);
+    }
+
+}
--- src/test/java/com/baro/SingleRentalTest.java
+++ src/test/java/com/baro/SingleRentalTest.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-public class RentalTest {
+public class SingleRentalTest {
 
     @Test(expected = IllegalRentalException.class)
     public void createIllegalSingleRentalNegativeDuration() throws IllegalRentalException{
@@ -24,29 +21,8 @@ public class RentalTest {
         new SingleRental(startTime, startTime);
     }
 
-    @Test(expected = IllegalRentalException.class)
-    public void createIllegalFamilyRentalByInsufficiency() throws IllegalRentalException {
-        Instant startTime = Instant.now();
-        List<SingleRental> rentals = new ArrayList<>();
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        new FamilyRental(rentals);
-    }
-
-    @Test(expected = IllegalRentalException.class)
-    public void createIllegalFamilyRentalByExcess() throws IllegalRentalException {
-        Instant startTime = Instant.now();
-        List<SingleRental> rentals = new ArrayList<>();
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        rentals.add(new SingleRental(startTime, startTime.plus(1, ChronoUnit.MINUTES)));
-        new FamilyRental(rentals);
-    }
-
     @Test
-    public void accurateSingleRentalPricing() throws IllegalRentalException {
+    public void accurateSingleRentalPricingHourly() throws IllegalRentalException {
         Instant startTime = Instant.now();
         SingleRental rental;
 
@@ -66,17 +42,30 @@ public class RentalTest {
         rental = new SingleRental(startTime, startTime.plus(23, ChronoUnit.HOURS)
                                                     .plus(59, ChronoUnit.MINUTES));
         assertEquals(24d * 5d, rental.getPrice(), 0d);
+    }
+
+    @Test
+    public void accurateSingleRentalPricingDaily() throws IllegalRentalException {
+        Instant startTime = Instant.now();
+        SingleRental rental;
 
         rental = new SingleRental(startTime, startTime.plus(1, ChronoUnit.DAYS));
         assertEquals(20d, rental.getPrice(), 0d);
 
-        rental = new SingleRental(startTime, startTime.plus(7, ChronoUnit.DAYS));
-        assertEquals(60d, rental.getPrice(), 0d);
+        rental = new SingleRental(startTime, startTime.plus(6, ChronoUnit.DAYS)
+                                                    .plus(10, ChronoUnit.HOURS));
+        assertEquals(7d * 20d, rental.getPrice(), 0d);
     }
 
     @Test
-    public void accurateFamilyRentalPricing() throws IllegalRentalException {
-        //TODO
-        assertTrue(false);
+    public void accurateSingleRentalPricingWeekly() throws IllegalRentalException {
+        Instant startTime = Instant.now();
+        SingleRental rental;
+
+        rental = new SingleRental(startTime, startTime.plus(7, ChronoUnit.DAYS));
+        assertEquals(60d, rental.getPrice(), 0d);
+
+        rental = new SingleRental(startTime, startTime.plus(8, ChronoUnit.DAYS));
+        assertEquals(2d * 60d, rental.getPrice(), 0d);
     }
 }
